27eef09ede79d9eebfd8e444bb664c569b01a640
unknown <hariprasanth431@gmail.com>
2022-02-09 12:51:48 +0530

    format

--- routes/complaint.js
+++ routes/complaint.js
@@ -4,7 +4,7 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 const router = express.Router();
-const {complaintModel, complaintChatModel} = require('../models/complaints');
+const { complaintModel, complaintChatModel } = require('../models/complaints');
 
 // app.get('/complaint',(req,res)=>{
 //     const complaint = new complaintModel({
@@ -25,54 +25,54 @@ const {complaintModel, complaintChatModel} = require('../models/complaints');
 //     });
 // });
 
-router.post('/fetchComplaint',(req,res)=>{
-    const {cid,password} = req.body;
-    complaintModel.findOne({cid:cid},(err,complaint)=>{
-        if(err){
+router.post('/fetchComplaint', (req, res) => {
+    const { cid, password } = req.body;
+    complaintModel.findOne({ cid: cid }, (err, complaint) => {
+        if (err) {
             res.send(err);
         }
-        if(bcrypt.compareSync(password,complaint.password)){
+        if (bcrypt.compareSync(password, complaint.password)) {
             const token = jwt.sign({
-                cid:complaint.cid,
-                user_role:"student"
-            },process.env.SECRET_KEY,{
+                cid: complaint.cid,
+                user_role: "student"
+            }, process.env.SECRET_KEY, {
                 expiresIn: '1h'
             });
             res.json({
-                complaint:complaint,
-                token:token
+                complaint: complaint,
+                token: token
             });
         }
-        else{
+        else {
             res.send('Invalid Complaint Id or Password');
         }
     });
 });
 
 //TODO: auth should be added
-router.post('/sendMessage', (req, res)=>{
+router.post('/sendMessage', (req, res) => {
     const body = req.body;
 
     //TODO: mocking user id
     user = "1";
 
-    if(body.cid === null || body.cid === undefined){
+    if (body.cid === null || body.cid === undefined) {
         res.send({
             success: false,
             msg: 'cid is missing'
         });
-    }else if(body.message === null || body.message === undefined){
+    } else if (body.message === null || body.message === undefined) {
         res.send({
             success: false,
             msg: 'message is missing'
         });
-    }else{
-        complaintModel.findOne({cid: body.cid}, (err, complaint)=> {
-            if(!err) {
-                if(!complaint) {
+    } else {
+        complaintModel.findOne({ cid: body.cid }, (err, complaint) => {
+            if (!err) {
+                if (!complaint) {
                     console.log("No such complaint exist");
-                    res.send({ success:false });
-                }else{
+                    res.send({ success: false });
+                } else {
                     currentChat = new complaintChatModel({
                         user: user,
                         message: body.message,
@@ -80,18 +80,18 @@ router.post('/sendMessage', (req, res)=>{
                     });
                     complaint.chatHistory.push(currentChat);
                 }
-                complaint.save(function(err) {
-                    if(!err) {
-                        res.send({ success:true });
+                complaint.save(function (err) {
+                    if (!err) {
+                        res.send({ success: true });
                     }
                     else {
                         console.log("Error at updating complaint: " + complaint);
-                        res.send({ success:false });
+                        res.send({ success: false });
                     }
                 });
-            }else{
+            } else {
                 console.log("Error at updating complaint: " + complaint);
-                res.send({ success:false });
+                res.send({ success: false });
             }
         });
     }
